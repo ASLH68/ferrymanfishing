cb91af46b4a7bb1103b88be73fd930087a58ff8f
Johnatan Vargas <jvargas@mail.bradley.edu>
2024-01-26 15:20:22 -0600

    Code Header

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,25 @@
+/******************************************************************
+*    Author: Johnatan Vargas
+*    Contributors: 
+*    Date Created: 1/26/2024
+*    Description: ui controller
+*******************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

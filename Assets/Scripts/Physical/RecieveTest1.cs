908fd436efb87c834b2875e2e5ba0abcf3688aa9
Rudy <rwolfer@mail.bradley.edu>
2024-02-02 14:54:47 -0600

    Adjustments

--- Assets/Scripts/Physical/RecieveTest1.cs
+++ Assets/Scripts/Physical/RecieveTest1.cs
@@ -13,9 +13,27 @@ public class RecieveTest1 : MonoBehaviour
     private void Start()
     {
         _a = Arduino.Instance;
+
+        StartCoroutine(MotorSpinCoroutine());
     }
 
     private void Update()
+    {
+
+    }
+
+    private IEnumerator MotorSpinCoroutine()
+    {
+        while (true)
+        {
+            SpinMotor();
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    [ContextMenu("Invoke Spin")]
+    private void SpinMotor()
     {
         _a.GetThread().WriteMessageThreaded($"{_forwardsRot}|{_backwardsRot}");
 

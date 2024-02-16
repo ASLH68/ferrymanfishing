9b5e21426bf3d68c3f9203bd97bdbd1bfd09b426
Johnatan Vargas <jvargas@mail.bradley.edu>
2024-02-16 13:40:28 -0600

    Intro Screen
    fixed the sizing issue (hopefully)  and commented the intro scene script

--- Assets/Scripts/IntroSceneBehavior.cs
+++ Assets/Scripts/IntroSceneBehavior.cs
@@ -7,7 +7,10 @@ public class IntroSceneBehavior : MonoBehaviour
 {
     public static IntroSceneBehavior Instance;
     [SerializeField]private GameObject _castScreen;
-
+    /// <summary>
+    /// if the casting screen is still acitve it will stay but if cast is pressed then the casting screen should 
+    /// disappear
+    /// </summary>
     private void Awake()
     {
         if(Instance == null)
@@ -19,7 +22,9 @@ public class IntroSceneBehavior : MonoBehaviour
             Destroy(this);
         }
     }
-
+    /// <summary>
+    /// At the start of the game this sets the cast screen to being true 
+    /// </summary>
     // Start is called before the first frame update
     void Start()
     {
@@ -31,7 +36,10 @@ public class IntroSceneBehavior : MonoBehaviour
     {
         
     }
-
+    /// <summary>
+    /// this to set and call CastingScreen in the Gameskeleton to set the cast screen to false 
+    /// </summary>
+    /// <param name="val"></param>
     public void CastingScreen(bool val)
     {
         _castScreen.gameObject.SetActive(val);

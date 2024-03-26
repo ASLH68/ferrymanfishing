bf6c366b53f5dc105aa92135ea0ba1f4d10d8dfc
AndreaXyna <andreaswihartdecoster@gmail.com>
2024-03-26 17:47:22 -0500

    UI Changes
    Be Gentle intro screen. fish name cards, new fish caught images without white space, removed UI text

--- Assets/Scripts/IntroSceneBehavior.cs
+++ Assets/Scripts/IntroSceneBehavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,16 +8,19 @@ using UnityEngine.UI;
 public class IntroSceneBehavior : MonoBehaviour
 {
     private Image _introImage;
+    private Image _gentleImage;
     public static IntroSceneBehavior Instance;
-    [SerializeField]private GameObject _castScreen;
-   
+    [SerializeField] private GameObject _castScreen;
+    [SerializeField] private GameObject _gentleScreen;
+    [SerializeField] private float _gentleScreenDur;
+
     /// <summary>
     /// if the casting screen is still acitve it will stay but if cast is pressed then the casting screen should 
     /// disappear
     /// </summary>
     private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
         }
@@ -32,16 +36,10 @@ public class IntroSceneBehavior : MonoBehaviour
     void Start()
     {
         _castScreen.gameObject.SetActive(true);
-        _introImage = GetComponentInChildren<Image>();
-        
+        _introImage = transform.GetChild(0).GetComponent<Image>();
+        _gentleImage = transform.GetChild(1).GetComponent<Image>();
     }
 
-  
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     /// <summary>
     /// this to set and call CastingScreen in the Gameskeleton to make intro scene fadeout 
     /// </summary>
@@ -49,31 +47,55 @@ public class IntroSceneBehavior : MonoBehaviour
     public void CastingScreen(bool val)
     {
         StartFading();
-        
     }
-    
+
     /// <summary>
     /// This makes the Intro Scene Fadeout Once it gets called
     /// </summary>
     /// <returns></returns>
-    IEnumerator FadeOut()
+    IEnumerator FadeOut(Image img)
     {
         for (float f = 1f; f >= -0.05f; f -= 0.05f)
         {
-            Color c = _introImage.color;
+            Color c = img.color;
             c.a = f;
-            _introImage.color= c;
+            img.color = c;
             yield return new WaitForSeconds(0.05f);
         }
-        _castScreen.gameObject.SetActive(false);
+        img.gameObject.SetActive(false);
+
+        if (img.Equals(_introImage))
+        {
+            StartCoroutine(FadeInGentleImg());
+        } 
+        else if (img.Equals(_gentleImage))
+        {
+            GameSkeleton.Instance.TriggerCast();
+        }
     }
+
     /// <summary>
     /// This coroutine calls for FadeOut
     /// </summary>
     public void StartFading()
     {
-        StartCoroutine("FadeOut");
-        
+        StartCoroutine(FadeOut(_introImage));
     }
 
+    /// <summary>
+    /// Fades in the be gentle img
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator FadeInGentleImg()
+    {
+        for (float f = 0f; f <= 1f; f += 0.05f)
+        {
+            Color c = _gentleImage.color;
+            c.a = f;
+            _gentleImage.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return new WaitForSeconds(_gentleScreenDur);
+        StartCoroutine(FadeOut(_gentleImage));
+    }
 }
